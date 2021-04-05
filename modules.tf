190dd1c66c42806d12416f0e141e46dc8e2a1c01
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:36:45 -0400

    Create modules.tf

---
+++ modules.tf
@@ -0,0 +1,10 @@
+//--------------------------------------------------------------------
+// Modules
+module "compute_instance" {
+  source  = "app.terraform.io/Benchmark/compute_instance/google"
+  version = "0.0.3"
+
+  amount = 1
+  image = "ubuntu-20.04-focal-v20210325"
+  name_prefix = "telus"
+}
