538d070a8664955c1ffe06e4df6f8dcb236e44a1
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 12:46:38 -0400

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -6,11 +6,12 @@ variable "compute_instance_project" {}
 // Modules
 module "compute_instance" {
   source  = "app.terraform.io/Benchmark/compute_instance/google"
-  version = "0.0.4"
+  version = "0.0.6"
   amount = 1
   image = "ubuntu-os-cloud/ubuntu-2004-lts"
   name_prefix = "telus"
   project = var.compute_instance_project
-  subnetwork = "manage-vpc"
+  network = "manage-vpc"
+  subnetwork = "manage-subnet-01"
   zone = "us-east1-b"
 }
