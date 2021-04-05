52cdb3ed3ff7bee1593017d1bac9ef62d2963409
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 12:38:05 -0400

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -11,6 +11,6 @@ module "compute_instance" {
   image = "ubuntu-os-cloud/ubuntu-2004-lts"
   name_prefix = "telus"
   project = var.compute_instance_project
-  subnetwork = "manage-vpc
+  subnetwork = "manage-vpc"
   zone = "us-east1-b"
 }
