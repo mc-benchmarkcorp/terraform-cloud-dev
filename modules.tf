5f9f8327e9ece7c95162a769202ccf1dcba29800
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:41:10 -0400

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -2,7 +2,7 @@
 // Modules
 module "compute_instance" {
   source  = "app.terraform.io/Benchmark/compute_instance/google"
-  version = "0.0.3"
+  version = "0.0.4"
 
   amount = 1
   image = "ubuntu-20.04-focal-v20210325"
