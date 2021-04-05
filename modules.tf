6cc01d5a5994b16ead1201bdb6ba71150218e5ca
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:44:08 -0400

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -3,7 +3,10 @@
 module "compute_instance" {
   source  = "app.terraform.io/Benchmark/compute_instance/google"
   version = "0.0.4"
-
+  subnetwork   = "subnet-01"
+  zone         = "us-east1-b"
+  tags = "ssh"
+  machine_type = "n1-standard-1"
   amount = 1
   image = "ubuntu-20.04-focal-v20210325"
   name_prefix = "telus"
