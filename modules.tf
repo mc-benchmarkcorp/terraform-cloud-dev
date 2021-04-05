069188af1bee6cae1d942e8d35cf0d0e03117e8c
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 12:42:28 -0400

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -6,7 +6,7 @@ variable "compute_instance_project" {}
 // Modules
 module "compute_instance" {
   source  = "app.terraform.io/Benchmark/compute_instance/google"
-  version = "0.0.3"
+  version = "0.0.4"
   amount = 1
   image = "ubuntu-os-cloud/ubuntu-2004-lts"
   name_prefix = "telus"
