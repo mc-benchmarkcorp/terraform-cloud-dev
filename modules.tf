ed118f5801acb099df7a3170e815d90df68da6a9
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:49:14 -0400

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -2,7 +2,7 @@
 // Modules
 module "compute_instance" {
   source  = "app.terraform.io/Benchmark/compute_instance/google"
-  version = "0.0.4"
+  version = "0.0.5"
   subnetwork   = "subnet-01"
   zone         = "us-east1-b"
   tags = "ssh"
